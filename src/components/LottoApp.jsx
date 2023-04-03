48a4f14efc37f45134aeed7d7346f13b99ea05fe
Marcin Noga <marcin@careaboutit.pl>
2023-04-03 09:49:33 +0200

    added router

--- src/components/LottoApp.jsx
+++ src/components/LottoApp.jsx
@@ -1,9 +1,21 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 import HomePage from "../pages/HomePage";
+import GamePage from "../pages/GamePage";
+import ResultPage from "../pages/ResultPage";
+import ErrorComponent from "./ErrorComponent";
 
 export default function LottoApp() {
     return (
         <div className="LottoApp">
-            <HomePage/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<HomePage/>}></Route>
+                    <Route path='/game' element={<GamePage/>}></Route>
+                    <Route path='/result' element={<ResultPage/>}></Route>
+                    <Route path='*' element={<ErrorComponent/>}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
