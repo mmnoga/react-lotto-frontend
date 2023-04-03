8171963f5b1d9a61c97cf899770323ec5ce8dc07
Marcin Noga <marcin@careaboutit.pl>
2023-04-03 10:58:51 +0200

    added form for input numbers

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,10 +1,38 @@
 export default function GamePage() {
     return (
         <div className="GamePage">
-            <h1>Game Page</h1>
-            <div>
-                Game Page Content...
-            </div>
+            <h1>Lotto Game</h1>
+            <form>
+                <div className="row">
+                    <div className="col">
+                        <div>Input 6 numbers (from 1 to 99):</div>
+                    </div>
+                </div>
+                <div className="row align-items-center">
+                    <div className="col">
+                        <input type="number" className="form-control" placeholder="#1"/>
+                    </div>
+                    <div className="col">
+                        <input type="number" className="form-control" placeholder="#2"/>
+                    </div>
+                    <div className="col">
+                        <input type="number" className="form-control" placeholder="#3"/>
+                    </div>
+                    <div className="col">
+                        <input type="number" className="form-control" placeholder="#4"/>
+                    </div>
+                    <div className="col">
+                        <input type="number" className="form-control" placeholder="#5"/>
+                    </div>
+                    <div className="col">
+                        <input type="number" className="form-control" placeholder="#6"/>
+                    </div>
+                    <div className="col">
+                        <button className="btn btn-success">Generate Ticket</button>
+                    </div>
+                </div>
+
+            </form>
         </div>
     )
 }
