d39864f504520ac16fc65807b70a266667da5b04
Marcin Noga <marcin@careaboutit.pl>
2023-04-08 16:14:52 +0100

    added game rules on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,21 @@
+import {Link} from "react-router-dom";
+
 export default function HomePage() {
     return (
-        <div className="HomePage">
-            <h1>Lotto Game Application</h1>
-            <div>
-                Try you luck and play Lotto Game.
+        <>
+            <div className="HomePage">
+                <h1>Lotto Game Application</h1>
+                <h4>Rules</h4>
+                <p>Player chooses 6 different numbers in range 1 to 99. System returns ticket with ID and next draw
+                    date.</p>
+                <p>Draw numbers take place at 12.oo on every Saturday.</p>
+                <p>Player <Link to="/result">checks game results</Link> using ticket ID.</p>
+                <h5>Try your luck and <Link to="/game">play Lotto Game!</Link></h5>
             </div>
-        </div>
+            <div className="alert alert-info">
+                You can test application and check game results using IDs: 3f5b7169-4b88-4978-919a-3d857389c6ea
+                or dfe54e5d-37af-403a-a431-be98af44eca0
+            </div>
+        </>
     )
 }
