a2d1112580d740910bd5867fe2d0c4048d0544d6
Marcin Noga <marcin@careaboutit.pl>
2023-04-17 16:04:51 +0200

    added deploy configuration

--- src/api/ApiService.js
+++ src/api/ApiService.js
@@ -1,40 +1,11 @@
-// import {useEffect, useState} from "react";
-// import axios from "axios";
-//
-// import {apiClient} from "./ApiClient";
-//
-// export const useFetch = (method, url) => {
-//     const [data, setData] = useState(null)
-//     const [isLoading, setIsLoading] = useState(true)
-//     const [error, setError] = useState(null)
-//
-//     useEffect(() => {
-//         axios({
-//             url,
-//             method: method
-//         })
-//             .then(response => {
-//                 setData(response.data)
-//                 setIsLoading(false)
-//             })
-//             .catch(error => {
-//                 console.log(error.message)
-//                 setError(error)
-//                 setIsLoading(false)
-//             });
-//     }, [url])
-//
-//     return {data, isLoading, error}
-// }
-
-
 import {apiClient} from "./ApiClient";
+import axios from "axios";
 
 const createTicket =
-    (numbers) => apiClient.post(`/inputNumbers?numbers=${numbers}`)
+    (numbers) => axios.post(`/api/inputNumbers?numbers=${numbers}`)
 
 const checkResult =
-    (ticketId) => apiClient.get(`/winners/${ticketId}`)
+    (ticketId) => axios.get(`/api/winners/${ticketId}`)
 
 const ApiService = {createTicket, checkResult}
 
